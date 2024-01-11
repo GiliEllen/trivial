81548eb47eaa5064a66dca4a6cb4ed3152ed2578
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2024-01-11 11:27:02 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import "dotenv/config";
 import { createServer } from "http";
 import express from "express";
+import cookieParser from "cookie-parser";
+import { json } from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
+
+app.use(cookieParser());
+app.use(json());
 
 app.get("/api/hello", (req, res) => {
     res.send("world");
@@ -12,4 +19,16 @@ app.use(express.static("public"));
 const server = createServer(app);
 const port = process.env.PORT ?? 3000;
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+async function init() {
+    if (!process.env.MONGO_CONNECTION_STRING) {
+        throw new Error("Must provide connection string for mongodb");
+    }
+
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+        dbName: "app-db-name"
+    });
+
+    server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+}
+
+init();
